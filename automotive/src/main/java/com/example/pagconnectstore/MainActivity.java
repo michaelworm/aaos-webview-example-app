67138c81ccf0514d2e02a2dc7c2ec8a18162c7c6
Michael Worm <michael.worm@mhp.com>
2023-05-22 16:20:05 +0200

    Initial commit

---
+++ automotive/src/main/java/com/example/pagconnectstore/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.pagconnectstore;
+
+import android.annotation.SuppressLint;
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://connect-store2.porsche.com/de/de/landauswahl/t/countrySelector");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        webView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        webView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        webView.destroy();
+    }
+}
