166ba99cda78389446ea17570f985ea3ced1b3be
Oguzhan Yüksel <oguzhan.yueksel@mhp.com>
2023-09-25 14:15:46 +0200

    add WebView settings

--- automotive/src/main/java/com/example/pagconnectstore/MainActivity.java
+++ automotive/src/main/java/com/example/pagconnectstore/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.pagconnectstore;
 
 import android.annotation.SuppressLint;
-import android.net.http.SslError;
 import android.os.Bundle;
-import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -13,19 +11,25 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private static final String CONNECT_STORE_URL = "https://connect-store2.porsche.com/de/de/landauswahl/t/countrySelector";
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureWebView();
+    }
 
+    private void configureWebView () {
         webView = findViewById(R.id.webview);
         WebSettings webSettings = webView.getSettings();
-
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webSettings.setDomStorageEnabled(true);
         webSettings.setJavaScriptEnabled(true);
-
-        webView.loadUrl("https://connect-store2.porsche.com/de/de/landauswahl/t/countrySelector");
+        webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        webView.loadUrl(MainActivity.CONNECT_STORE_URL);
+        webView.setWebViewClient(new WebViewClient());
     }
 
     @Override
